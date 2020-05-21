ae17889f82ea90335b4cb7aad346d1731acde7db
mickoangelolacap <mickoangelolacap@gmail.com>
2020-05-21 16:17:13 +0800

    Add changes config/app.php

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:jL2lLVpCut5TSXyFModY6s5FFzlqLEceeLQ5Rv+dzrQ='),
 
     'cipher' => 'AES-256-CBC',
 
